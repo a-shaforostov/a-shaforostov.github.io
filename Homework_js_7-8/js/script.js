65bfa680b694fc1c8897b0f22460c99e64d91f1e
a-shaforostov <andpr99@gmail.com>
2016-04-06 17:55:16 +0300

    Домашняя работа 7-8

---
+++ Homework_js_7-8/js/script.js
@@ -0,0 +1,48 @@
+$(function() {
+
+    // ----- Закладки -----
+    var $tabs = $('.tab');
+
+    $tabs.on('click', function(e) {
+        // Не обрабатывать нажатие на активной закладке
+        if (this.classList.contains('active-tab')) return;
+
+        // Получить активную вкладку и ее номер
+        var $activeTab = $('.active-tab');
+        var ti = $activeTab.attr('data-tab-index');
+
+        // Сделать ее неактивной и спрятать текст
+        $('.tab-body[data-tab-index="'+ti+'"]').slideToggle();
+        $('.active-tab').removeClass('active-tab');
+
+        // Сделать активной текущую вкладку
+        $activeTab = $(this);
+        $activeTab.addClass('active-tab');
+
+        ti = $activeTab.attr('data-tab-index');
+        $('.tab-body[data-tab-index="'+ti+'"]').slideToggle();
+
+        e.preventDefault();
+    });
+
+    // ----- Подсказки -----
+    var $form = $('.form');
+    var $formFS = $form.find('.fieldset');
+
+    $formFS.on('focusin mouseenter', 'input', function(e) {
+        var $hint = $(e.target.parentNode).find('.hint');
+        $hint.fadeIn();
+    });
+
+    $formFS.on('focusout mouseleave', 'input', function(e) {
+        // Если элемент в фокусе - не убирать подсказку
+        if (e.target === document.activeElement) return;
+
+        var $hint = $(e.target.parentNode).find('.hint');
+        $hint.stop().fadeOut();
+    });
+
+    $form.on('click', '#show-help',function(e) {
+        $('.hint').stop().fadeIn();
+    });
+});
