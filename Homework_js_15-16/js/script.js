15d2390789810271e9964cb01e94c8c95aea485a
a-shaforostov <andpr99@gmail.com>
2016-05-04 19:12:11 +0300

    FB

--- Homework_js_15-16/js/script.js
+++ Homework_js_15-16/js/script.js
@@ -10,13 +10,33 @@ function GoogleCallback(func, data){
 
         $('.search-form form').on('submit', function(e) {
 
+            //$.ajax({
+            //    url: "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q="+encodeURIComponent(this.elements[0].value)+"&hl=en&rsz=large&callback=GoogleCallback&context=?",
+            //    dataType: "jsonp",
+            //    success: function (data) {
+            //        if (data === null) {
+            //            $('#results').html("Ошибка");
+            //        } else {
+            //            // Преобразовать шаблон в HTML и поместить его в DOM
+            //            var template = $('#search-response').html();
+            //            var html = tmpl(template, data);
+            //            $('#results').html(html);
+            //            console.log(data);
+            //        }
+            //    }
+            //});
+            //event.preventDefault();
+
             $.ajax({
-                url: "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q="+encodeURIComponent(this.elements[0].value)+"&hl=en&rsz=large&callback=GoogleCallback&context=?",
+
+                url: "http://search.yahooapis.com/WebSearchService/V1/webSearch?appid=YahooDemo&output=json&query="+encodeURIComponent(this.elements[0].value)+"&callback=?",
                 dataType: "jsonp",
                 success: function (data) {
                     if (data === null) {
                         $('#results').html("Ошибка");
                     } else {
+                        console.log(arguments);
+                        debugger;
                         // Преобразовать шаблон в HTML и поместить его в DOM
                         var template = $('#search-response').html();
                         var html = tmpl(template, data);
