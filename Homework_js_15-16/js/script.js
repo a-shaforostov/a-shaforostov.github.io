6e948bc1abe62d2a7b2c9ecc83df95a42fe8e362
a-shaforostov <andpr99@gmail.com>
2016-05-04 11:13:39 +0300

    Google API 2

--- Homework_js_15-16/js/script.js
+++ Homework_js_15-16/js/script.js
@@ -1,40 +1,34 @@
-function GoogleCallback(){
-    console.log("arguments", arguments);
+function GoogleCallback(func, data){
+    console.log(arguments);
+    window[func](data);
 }
 
-//(function($) {
-    //"use strict";
-
-// callback function
-//function GoogleCallback (func, data) {
-//    window[func](data);
-//}
-//
-//$.getJSON("http://ajax.googleapis.com/ajax/services/search/web?v=1.0?key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=javascript&callback=GoogleCallback&context=?",
-//    function(data){
-//        var ul = document.createElement("ul");
-//        $.each(data.results, function(i, val){
-//            var li = document.createElement("li");
-//            li.innerHTML = '<a href="'+val.url+'" title="'+val.url+'" target="_blank">'+val.title+"</a> - "+val.content;
-//            ul.appendChild(li);
-//        });
-//        $('body').html(ul);
-//    });
+(function($) {
+    "use strict";
 
     $(function(){
-        $.ajax({
-            url: "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=javascript&callback=GoogleCallback&context=?",
-            dataType: "jsonp"
+
+        $('.search-form form').on('submit', function(e) {
+
+            $.ajax({
+                url: "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q="+encodeURIComponent(this.elements[0].value)+"&hl=en&rsz=large&callback=GoogleCallback&context=?",
+                dataType: "jsonp",
+                success: function (data) {
+                    if (data === null) {
+                        $('#results').html("Ошибка");
+                    } else {
+                        // Преобразовать шаблон в HTML и поместить его в DOM
+                        var template = $('#search-response').html();
+                        var html = tmpl(template, data);
+                        $('#results').html(html);
+                        console.log(data);
+                    }
+                }
+            });
+            event.preventDefault();
         });
+
 
     });
 
-//})(jQuery);
-
-
-//success: function () {
-//    alert("Success");
-//},
-//error: function () {
-//    alert("Error");
-//}
+})(jQuery);
